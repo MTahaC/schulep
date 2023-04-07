012227e1a649abe7afd63f7256ca4660eefc201a
MTahaC <mtcinar2022@gmail.com>
2023-04-07 16:38:13 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   mobil.js
    	modified:   style.css

--- mobil.js
+++ mobil.js
@@ -41,70 +41,70 @@
 
 //! A satir start
 function add1() {
-    a = parseInt(document.getElementById('a').value);
-    a1 = parseInt(document.getElementById('a1').value);
-    a2 = parseInt(document.getElementById('a2').value);
-    a3 = parseInt(document.getElementById('a3').value);
-    a4 = parseInt(document.getElementById('a4').value);
-    a5 = parseInt(document.getElementById('a5').value);
+    a = Number(document.getElementById('a').value);
+    a1 = Number(document.getElementById('a1').value);
+    a2 = Number(document.getElementById('a2').value);
+    a3 = Number(document.getElementById('a3').value);
+    // a4 = parseInt(document.getElementById('a4').value);
+    a5 = Number(document.getElementById('a5').value);
 
-    z1 = a * a1 * a2 * a3 * a4 * a5;
+    z1 = a * a1 * a2 * a3 * a5;
     document.getElementById('z1').value = z1;
 }
 //! A satir end
 
 //! B satir start
 function add2() {
-    b = parseInt(document.getElementById('b').value);
-    b1 = parseInt(document.getElementById('b1').value);
-    b2 = parseInt(document.getElementById('b2').value);
-    b3 = parseInt(document.getElementById('b3').value);
-    b4 = parseInt(document.getElementById('b4').value);
-    b5 = parseInt(document.getElementById('b5').value);
+    b = Number(document.getElementById('b').value);
+    b1 = Number(document.getElementById('b1').value);
+    b2 = Number(document.getElementById('b2').value);
+    b3 = Number(document.getElementById('b3').value);
+    // b4 = parseInt(document.getElementById('b4').value);
+    b5 = Number(document.getElementById('b5').value);
 
-    z2 = b * b1 * b2 * b3 * b4 * b5;
+    z2 = b * b1 * b2 * b3 * b5;
     document.getElementById('z2').value = z2;
 }
 //! B satir end
 
 //! C satir start
 function add3() {
-    c = parseInt(document.getElementById('c').value);
-    c1 = parseInt(document.getElementById('c1').value);
-    c2 = parseInt(document.getElementById('c2').value);
-    c3 = parseInt(document.getElementById('c3').value);
-    c4 = parseInt(document.getElementById('c4').value);
-    c5 = parseInt(document.getElementById('c5').value);
+    c = Number(document.getElementById('c').value);
+    c1 = Number(document.getElementById('c1').value);
+    c2 = Number(document.getElementById('c2').value);
+    c3 = Number(document.getElementById('c3').value);
+    // c4 = parseInt(document.getElementById('c4').value);
+    c5 = Number(document.getElementById('c5').value);
 
-    z3 = c * c1 * c2 * c3 * c4 * c5;
+    z3 = c * c1 * c2 * c3 * c5;
     document.getElementById('z3').value = z3;
 }
 //! C satir end
 
 //! D satir start
 function add4() {
-    d = parseInt(document.getElementById('d').value);
-    d1 = parseInt(document.getElementById('d1').value);
-    d2 = parseInt(document.getElementById('d2').value);
-    d3 = parseInt(document.getElementById('d3').value);
-    d4 = parseInt(document.getElementById('d4').value);
-    d5 = parseInt(document.getElementById('d5').value);
+    d = Number(document.getElementById('d').value);
+    d1 = Number(document.getElementById('d1').value);
+    d2 = Number(document.getElementById('d2').value);
+    d3 = Number(document.getElementById('d3').value);
+    // d4 = parseInt(document.getElementById('d4').value);
+    d5 = Number(document.getElementById('d5').value);
 
-    z4 = d * d1 * d2 * d3 * d4 * d5;
+    z4 = d * d1 * d2 * d3 * d5;
     document.getElementById('z4').value = z4;
 }
 //! D satir end
 
 //! E satir start
 function add5() {
-    e = parseInt(document.getElementById('e').value);
-    e1 = parseInt(document.getElementById('e1').value);
-    e2 = parseInt(document.getElementById('e2').value);
-    e3 = parseInt(document.getElementById('e3').value);
-    e4 = parseInt(document.getElementById('e4').value);
-    e5 = parseInt(document.getElementById('e5').value);
+    e = Number(document.getElementById('e').value);
+    e1 = Number(document.getElementById('e1').value);
+    e2 = Number(document.getElementById('e2').value);
+    e3 = Number(document.getElementById('e3').value);
+    // e4 = parseInt(document.getElementById('e4').value);
+    e5 = Number(document.getElementById('e5').value);
 
-    z5 = e * e1 * e2 * e3 * e4 * e5;
+    z5 = e * e1 * e2 * e3 * e5;
     document.getElementById('z5').value = z5;
 }
 //! E satir end
@@ -127,11 +127,11 @@ function rechnen(){
 //! X satir start
 
 function addx1() {
-    z1 = parseInt(document.getElementById('z1').value);
-    z2 = parseInt(document.getElementById('z2').value);
-    z3 = parseInt(document.getElementById('z3').value);
-    z4 = parseInt(document.getElementById('z4').value);
-    z5 = parseInt(document.getElementById('z5').value);
+    z2 = Number(document.getElementById('z2').value);
+    z3 = Number(document.getElementById('z3').value);
+    z5 = Number(document.getElementById('z5').value);
+    z4 = Number(document.getElementById('z4').value);
+    z1 = Number(document.getElementById('z1').value);
 
     x1 = z1 + z2 + z3 + z4 + z5;
     document.getElementById('x1').value = x1;
